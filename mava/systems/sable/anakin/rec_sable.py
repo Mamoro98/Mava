7e44446900c5fb52c973266b8af812608393bef8
Omer Kamal <endomamoro9@gmail.com>
2025-05-08 20:21:07 +0200

    add try and expect to handle resource exhasted error

--- mava/systems/sable/anakin/rec_sable.py
+++ mava/systems/sable/anakin/rec_sable.py
@@ -942,9 +942,13 @@ def hydra_entry_point(cfg: DictConfig) -> float:
     OmegaConf.set_struct(cfg, False)
 
     # Run experiment.
-    eval_performance = run_experiment(cfg)
-    print(f"{Fore.CYAN}{Style.BRIGHT}Rec Sable experiment completed{Style.RESET_ALL}")
-    return eval_performance
+    try:
+        eval_performance = run_experiment(cfg)
+        print(f"{Fore.CYAN}{Style.BRIGHT}Rec Sable experiment completed{Style.RESET_ALL}")
+        return eval_performance
+    except:
+        print(f"{Fore.BLACK}{Style.BRIGHT}Rec Sable experiment Could not run{Style.RESET_ALL}")
+        return -1
 
 
 if __name__ == "__main__":
