e24e307561102715d838156477c076dc393dc5f6
Omer Kamal <endomamoro9@gmail.com>
2025-05-27 10:34:13 +0200

    see the results of  new setup of shuffled hs

--- mava/systems/sable/anakin/rec_sable.py
+++ mava/systems/sable/anakin/rec_sable.py
@@ -388,40 +388,49 @@ def get_learner_fn(
                 prev_hs_shuffled.append(prev_hstates_new)
 
 
-            losses = []
-            for i in range(len(minibatches_list)):
-                batch_info = (*minibatches_list[i], prev_hs_minibatch_list[i])
-                # (params, opt_states, entropy_key), loss_info =_update_minibatch((params, opt_states, key), batch_info)
+            N_minibatches = config.system.num_minibatches
+            N_tasks = len(minibatches_list)
 
-                def _update_minibatch_for_task_i(carry, dummy):
-                                return _update_minibatch(carry, dummy,task_id=i)
-                
-                (params, opt_states, entropy_key), loss_info = jax.lax.scan(
-                    _update_minibatch_for_task_i,
-                    (params, opt_states, entropy_key),
-                    batch_info,
-                )
-                losses.append(loss_info)
+            epoch_total_loss_sum = { "total_loss": 0.0, "value_loss": 0.0, "actor_loss": 0.0, "entropy": 0.0 }
+            epoch_loss_count = 0
 
-            # take the mean over the task dimensions of the loss info
-            print(f'losses {type(losses)}')
-            total_losses = {}
-            # total_losses = {
-            #     k: sum(loss[k] for loss in losses) / len(losses)
-            #     for k in losses[0].keys()
-            #     }
-            for i in range(len(losses)):
-                for j in losses[i].keys():
-                    if j not in total_losses.keys():
-                        total_losses[j] = 0
-                    total_losses[j] = total_losses[j] + losses[i][j]
-            for i in total_losses.keys():
-                total_losses[i] = total_losses[i] / len(losses)
+            for i in range(N_minibatches):
+                for j in range(N_tasks):
+                        traj_data_all_mbs_for_task, adv_data_all_mbs_for_task, targets_data_all_mbs_for_task = minibatches_list[j]
+       
+                        #
+                        current_traj_data_mb = jax.tree_util.tree_map(
+                            lambda leaf_all_mbs: leaf_all_mbs[i],
+                            traj_data_all_mbs_for_task
+                        )
+                        current_adv_data_mb = adv_data_all_mbs_for_task[i]
+                        current_targets_data_mb = targets_data_all_mbs_for_task[i]
+                        
+                        
+                        hs_data_all_mbs_for_task = prev_hs_minibatch_list[j]
+                        current_hs_data_mb = jax.tree_util.tree_map(
+                            lambda leaf_all_mbs: leaf_all_mbs[i],
+                            hs_data_all_mbs_for_task
+                        )
+                        
+                        batch_info_single_mb_task = (current_traj_data_mb, current_adv_data_mb, current_targets_data_mb, current_hs_data_mb)
 
+                                   
+                        (params, opt_states, key), loss_info_one_task_one_mb = _update_minibatch(
+                            (params, opt_states, key), 
+                            batch_info_single_mb_task,
+                            task_id=j 
+                        )             
 
+                        
+                        for k_loss, v_loss in loss_info_one_task_one_mb.items():
+                            epoch_total_loss_sum[k_loss] += v_loss
+                        epoch_loss_count += 1 
+
+            final_epoch_avg_loss = {k: v / epoch_loss_count for k, v in epoch_total_loss_sum.items() if epoch_loss_count > 0}
 
             update_state = (params, opt_states, traj_batches_list, advantages_list, targets_list, key, prev_hs_shuffled)
-            return update_state, total_losses
+            return update_state, final_epoch_avg_loss
         
         # until here, i have all the info i need for the update, i have the adv, the targets, the traj_batches, .. everything
         # i need to update the params and opt_state now
@@ -543,7 +552,7 @@ def learner_setup(
             optax.adam(lr, eps=1e-5),
         ),
         # gradient accumilation happens every num_tasks -> the opt will step every n_tasks
-        every_k_schedule=  config.system.num_minibatches,
+        every_k_schedule=  num_tasks,
         # if true -> first we take the average of the graidents and then step using that avg -> emulate large batch size
         # if false -> gradients for each num_task step will be summed 
         use_grad_mean=True 
