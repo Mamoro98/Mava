2cceb08ee6371b1f7f096e077732fd729d6763a0
Omer Kamal <endomamoro9@gmail.com>
2025-05-30 15:37:02 +0200

    the same but for the reordered update

--- mava/systems/sable/anakin/rec_sable.py
+++ mava/systems/sable/anakin/rec_sable.py
@@ -361,50 +361,99 @@ def get_learner_fn(
                 minibatches_list.append(minibatches)
                 prev_hstates_list.append(prev_hstates_new)
 
-            # UPDATE MINIBATCHES
-            N_minibatches = config.system.num_minibatches
-            N_tasks = len(minibatches_list)
 
-            epoch_total_loss_sum = { "total_loss": 0.0, "value_loss": 0.0, "actor_loss": 0.0, "entropy": 0.0 }
-            epoch_loss_count = 0
 
-            for i in range(N_minibatches):
-                for j in range(N_tasks):
-                        traj_data_all_mbs_for_task, adv_data_all_mbs_for_task, targets_data_all_mbs_for_task = minibatches_list[j]
+            losses = [] 
+            for i in range(len(minibatches_list)):
+                batch_info = (*minibatches_list[i], prev_hs_minibatch_list[i])
+                # (params, opt_states, entropy_key), loss_info =_update_minibatch((params, opt_states, key), batch_info)
+
+                def _update_minibatch_for_task_i(carry, dummy):
+                                return _update_minibatch(carry, dummy,task_id=i)
+                
+                (params, opt_states, entropy_key), loss_info = jax.lax.scan(
+                    _update_minibatch_for_task_i,
+                    (params, opt_states, entropy_key),
+                    batch_info,
+                )
+                losses.append(loss_info)
+
+            # take the mean over the task dimensions of the loss info
+            print(f'losses {type(losses)}')
+            total_losses = {}
+            # total_losses = {
+            #     k: sum(loss[k] for loss in losses) / len(losses)
+            #     for k in losses[0].keys()
+            #     }
+            for i in range(len(losses)):
+                for j in losses[i].keys():
+                    if j not in total_losses.keys():
+                        total_losses[j] = 0
+                    total_losses[j] = total_losses[j] + losses[i][j]
+            for i in total_losses.keys():
+                total_losses[i] = total_losses[i] / len(losses)
+
+
+
+            update_state = (params, opt_states, traj_batches_list, advantages_list, targets_list, key, prev_hstates_list)
+            return update_state, total_losses
         
-                        #
-                        current_traj_data_mb = tree.map(
-                            lambda leaf_all_mbs: leaf_all_mbs[i],
-                            traj_data_all_mbs_for_task
-                        )
-                        current_adv_data_mb = adv_data_all_mbs_for_task[i]
-                        current_targets_data_mb = targets_data_all_mbs_for_task[i]
+
+
+
+
+
+
+
+
+
+
+
+
+            # # UPDATE MINIBATCHES
+            # N_minibatches = config.system.num_minibatches
+            # N_tasks = len(minibatches_list)
+
+            # epoch_total_loss_sum = { "total_loss": 0.0, "value_loss": 0.0, "actor_loss": 0.0, "entropy": 0.0 }
+            # epoch_loss_count = 0
+
+            # for i in range(N_minibatches):
+            #     for j in range(N_tasks):
+            #             traj_data_all_mbs_for_task, adv_data_all_mbs_for_task, targets_data_all_mbs_for_task = minibatches_list[j]
+        
+            #             #
+            #             current_traj_data_mb = tree.map(
+            #                 lambda leaf_all_mbs: leaf_all_mbs[i],
+            #                 traj_data_all_mbs_for_task
+            #             )
+            #             current_adv_data_mb = adv_data_all_mbs_for_task[i]
+            #             current_targets_data_mb = targets_data_all_mbs_for_task[i]
                         
                         
-                        hs_data_all_mbs_for_task = prev_hs_minibatch_list[j]
-                        current_hs_data_mb = tree.map(
-                            lambda leaf_all_mbs: leaf_all_mbs[i],
-                            hs_data_all_mbs_for_task
-                        )
+            #             hs_data_all_mbs_for_task = prev_hs_minibatch_list[j]
+            #             current_hs_data_mb = tree.map(
+            #                 lambda leaf_all_mbs: leaf_all_mbs[i],
+            #                 hs_data_all_mbs_for_task
+            #             )
                         
-                        batch_info_single_mb_task = (current_traj_data_mb, current_adv_data_mb, current_targets_data_mb, current_hs_data_mb)
+            #             batch_info_single_mb_task = (current_traj_data_mb, current_adv_data_mb, current_targets_data_mb, current_hs_data_mb)
 
                                     
-                        (params, opt_states, key), loss_info_one_task_one_mb = _update_minibatch(
-                            (params, opt_states, key), 
-                            batch_info_single_mb_task,
-                            task_id=j 
-                        )             
+            #             (params, opt_states, key), loss_info_one_task_one_mb = _update_minibatch(
+            #                 (params, opt_states, key), 
+            #                 batch_info_single_mb_task,
+            #                 task_id=j 
+            #             )             
 
                         
-                        for k_loss, v_loss in loss_info_one_task_one_mb.items():
-                            epoch_total_loss_sum[k_loss] += v_loss
-                        epoch_loss_count += 1 
+            #             for k_loss, v_loss in loss_info_one_task_one_mb.items():
+            #                 epoch_total_loss_sum[k_loss] += v_loss
+            #             epoch_loss_count += 1 
 
-            final_epoch_avg_loss = {k: v / epoch_loss_count for k, v in epoch_total_loss_sum.items() if epoch_loss_count > 0}
+            # final_epoch_avg_loss = {k: v / epoch_loss_count for k, v in epoch_total_loss_sum.items() if epoch_loss_count > 0}
 
-            update_state = (params, opt_states, traj_batches_list, advantages_list, targets_list, key, prev_hstates)
-            return update_state, final_epoch_avg_loss
+            # update_state = (params, opt_states, traj_batches_list, advantages_list, targets_list, key, prev_hstates)
+            # return update_state, final_epoch_avg_loss
 
 
 
