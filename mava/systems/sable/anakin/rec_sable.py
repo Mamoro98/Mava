96c8aa57b672c700c88ca602eb17db1272884693
Omer Kamal <endomamoro9@gmail.com>
2025-06-01 19:27:47 +0200

    change hyperparams
    This reverts commit 02cfe4f8d568561b389a591f2c19ab935f43dbff.

--- mava/systems/sable/anakin/rec_sable.py
+++ mava/systems/sable/anakin/rec_sable.py
@@ -250,7 +250,7 @@ def get_learner_fn(
         def _update_epoch(update_state: Tuple, _: Any) -> Tuple:
             """Update the network for a single epoch."""
 
-            def _update_minibatch(train_state: Tuple, batch_info: Tuple,task_id:int) -> Tuple:
+            def _update_minibatch(train_state: Tuple, batch_info: Tuple,task_id:int=0) -> Tuple:
                 """Update the network for a single minibatch."""
                 params, opt_state, key = train_state
                 traj_batch, advantages, targets, prev_hstates = batch_info
@@ -392,86 +392,111 @@ def get_learner_fn(
 
 
 
+            
 
-
-
-
-
-
-
-            # losses = []
-            # for i in range(len(minibatches_list)):
-            #     batch_info = (*minibatches_list[i], prev_hs_minibatch_list[i])
-
-            #     def _update_minibatch_for_task_i(carry, dummy):
-            #                     return _update_minibatch(carry, dummy,task_id=i)
-                
-            #     (params, opt_states, entropy_key), loss_info = jax.lax.scan(
-            #         _update_minibatch_for_task_i,
-            #         (params, opt_states, entropy_key),
-            #         batch_info,
-            #     )
-            #     losses.append(loss_info)
-
-            # # take the mean over the task dimensions of the loss info
-            # total_losses = {}
-           
-            # for i in range(len(losses)):
-            #     for j in losses[i].keys():
-            #         if j not in total_losses.keys():
-            #             total_losses[j] = 0
-            #         total_losses[j] = total_losses[j] + losses[i][j]
-            # for i in total_losses.keys():
-            #     total_losses[i] = total_losses[i] / len(losses)
-
-
+            # UPDATE MINIBATCHES
+            # (params, opt_states, entropy_key), loss_info = jax.lax.scan(
+            #     _update_minibatch,
+            #     (params, opt_states, entropy_key),
+            #     (*minibatches_list[0], prev_hs_minibatch_list[0]),
+            # )
 
             # update_state = (params, opt_states, traj_batches_list, advantages_list, targets_list, key, prev_hstates)
-            # return update_state, total_losses
-        
-            N_minibatches = config.system.num_minibatches
-            N_tasks = len(minibatches_list)
+            # return update_state, loss_info
 
-            epoch_total_loss_sum = { "total_loss": 0.0, "value_loss": 0.0, "actor_loss": 0.0, "entropy": 0.0 }
-            epoch_loss_count = 0
 
-            for i in range(N_minibatches):
-                for j in range(N_tasks):
-                        traj_data_all_mbs_for_task, adv_data_all_mbs_for_task, targets_data_all_mbs_for_task = minibatches_list[j]
-       
-                        
-                        current_traj_data_mb = tree.map(
-                            lambda leaf_all_mbs: leaf_all_mbs[i],
-                            traj_data_all_mbs_for_task
-                        )
-                        current_adv_data_mb = adv_data_all_mbs_for_task[i]
-                        current_targets_data_mb = targets_data_all_mbs_for_task[i]
-                        
-                        
-                        hs_data_all_mbs_for_task = prev_hs_minibatch_list[j]
-                        current_hs_data_mb = tree.map(
-                            lambda leaf_all_mbs: leaf_all_mbs[i],
-                            hs_data_all_mbs_for_task
-                        )
-                        
-                        batch_info_single_mb_task = (current_traj_data_mb, current_adv_data_mb, current_targets_data_mb, current_hs_data_mb)
 
-                                   
-                        (params, opt_states, entropy_key), loss_info_one_task_one_mb = _update_minibatch(
-                            (params, opt_states, entropy_key), 
-                            batch_info_single_mb_task,
-                            task_id=j 
-                        )             
 
-                        
-                        for k_loss, v_loss in loss_info_one_task_one_mb.items():
-                            epoch_total_loss_sum[k_loss] += v_loss
-                        epoch_loss_count += 1 
 
-            final_epoch_avg_loss = {k: v / epoch_loss_count for k, v in epoch_total_loss_sum.items() if epoch_loss_count > 0}
+
+            losses = []
+            for i in range(len(minibatches_list)):
+                batch_info = (*minibatches_list[i], prev_hs_minibatch_list[i])
+
+                def _update_minibatch_for_task_i(carry, dummy):
+                                return _update_minibatch(carry, dummy,task_id=i)
+                
+                (params, opt_states, entropy_key), loss_info = jax.lax.scan(
+                    _update_minibatch_for_task_i,
+                    (params, opt_states, entropy_key),
+                    batch_info,
+                )
+                losses.append(loss_info)
+
+            # take the mean over the task dimensions of the loss info
+            total_losses = {}
+           
+            for i in range(len(losses)):
+                for j in losses[i].keys():
+                    if j not in total_losses.keys():
+                        total_losses[j] = 0
+                    total_losses[j] = total_losses[j] + losses[i][j]
+            for i in total_losses.keys():
+                total_losses[i] = total_losses[i] / len(losses)
+
+
 
             update_state = (params, opt_states, traj_batches_list, advantages_list, targets_list, key, prev_hstates)
-            return update_state, final_epoch_avg_loss
+            return update_state, total_losses
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+            # N_minibatches = config.system.num_minibatches
+            # N_tasks = len(minibatches_list)
+
+            # epoch_total_loss_sum = { "total_loss": 0.0, "value_loss": 0.0, "actor_loss": 0.0, "entropy": 0.0 }
+            # epoch_loss_count = 0
+
+            # for i in range(N_minibatches):
+            #     for j in range(N_tasks):
+            #             traj_data_all_mbs_for_task, adv_data_all_mbs_for_task, targets_data_all_mbs_for_task = minibatches_list[j]
+       
+                        
+            #             current_traj_data_mb = tree.map(
+            #                 lambda leaf_all_mbs: leaf_all_mbs[i],
+            #                 traj_data_all_mbs_for_task
+            #             )
+            #             current_adv_data_mb = adv_data_all_mbs_for_task[i]
+            #             current_targets_data_mb = targets_data_all_mbs_for_task[i]
+                        
+                        
+            #             hs_data_all_mbs_for_task = prev_hs_minibatch_list[j]
+            #             current_hs_data_mb = tree.map(
+            #                 lambda leaf_all_mbs: leaf_all_mbs[i],
+            #                 hs_data_all_mbs_for_task
+            #             )
+                        
+            #             batch_info_single_mb_task = (current_traj_data_mb, current_adv_data_mb, current_targets_data_mb, current_hs_data_mb)
+
+                                   
+            #             (params, opt_states, entropy_key), loss_info_one_task_one_mb = _update_minibatch(
+            #                 (params, opt_states, entropy_key), 
+            #                 batch_info_single_mb_task,
+            #                 task_id=j 
+            #             )             
+
+                        
+            #             for k_loss, v_loss in loss_info_one_task_one_mb.items():
+            #                 epoch_total_loss_sum[k_loss] += v_loss
+            #             epoch_loss_count += 1 
+
+            # final_epoch_avg_loss = {k: v / epoch_loss_count for k, v in epoch_total_loss_sum.items() if epoch_loss_count > 0}
+
+            # update_state = (params, opt_states, traj_batches_list, advantages_list, targets_list, key, prev_hstates)
+            # return update_state, final_epoch_avg_loss
 
 
         
@@ -589,15 +614,16 @@ def learner_setup(
     optim = MultiSteps(
         # optax.chain -> it is like have a chain of optimizers -> sequentially executing the first one, then move to the next
         optax.chain(
-            # first transformation in the chain -> if the L2 norm of the entire gradient vector exceeds
+            # first transformation in the chain -> if the L2 norm    of the entire gradient vector exceeds
             # max_grad_norm -> clipping -> prevent exploding gradient 
             optax.clip_by_global_norm(config.system.max_grad_norm),
             # second transformation -> Adam optimizer
             optax.adam(lr, eps=1e-5),
         ),
         # gradient accumilation happens every num_tasks -> the opt will step every n_tasks
-        every_k_schedule = num_tasks,
-        # every_k_schedule = config.system.num_minibatches,
+        # every_k_schedule = num_tasks,
+        every_k_schedule = config.system.num_minibatches,
+        # every_k_schedule = 1,
         # if true -> first we take the average of the graidents and then step using that avg -> emulate large batch size
         # if false -> gradients for each num_task step will be summed 
         use_grad_mean=True 
